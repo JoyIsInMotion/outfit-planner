58a7bd4c4690f8d3c904865c6e38929c77d1e3c7
JoyIsInMotion <joyisinmotion@gmail.com>
2025-12-06 21:19:08 +0200

    Fix main.jsx imports and add / route for Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import './App.css'
 import Home from './pages/Home'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
 
 
   return (
     <>
-    <Home />
-      Outfit Planner
+
+    <Routes>
+    <Route path='/' element={<Home />} />
+    </Routes>
+
     </>
   )
 }
