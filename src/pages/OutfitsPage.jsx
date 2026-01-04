1f5add9f9957be12c0a89654025fee0a270eae83
JoyIsInMotion <joyisinmotion@gmail.com>
2026-01-04 17:48:00 +0200

    add static outfit page with list rendering

--- src/pages/OutfitsPage.jsx
+++ src/pages/OutfitsPage.jsx
@@ -1,10 +1,48 @@
 import React from 'react'
 
 function OutfitsPage() {
+  const outfits = [
+    {
+      id: 'outfit1',
+      name: 'Casual Look',
+      description: 'A comfortable and stylish casual outfit.',
+      imageUrl: 'https://example.com/casual-look.jpg'
+    },
+    {
+      id: 'outfit2',
+      name: 'Professional Look',
+      description: 'A stylish professional outfit.',
+      imageUrl: 'https://example.com/professional-look.jpg'
+    },
+    {
+      id: 'outfit3',
+      name: 'Sexy Look',
+      description: 'A sexy diva outfit.',
+      imageUrl: 'https://example.com/sexy-look.jpg'
+    }
+
+  ]
+
+  const outfitsList = outfits.map(outfit =>
+    <li key={outfit.id}>
+      <p>
+        {outfit.name}
+      </p>
+      <img src={outfit.imageUrl}
+        alt={outfit.name} />
+        <p>{outfit.description}</p>
+    </li>)
+
   return (
-    <div>
-     OutfitPage Works!
-    </div>
+    <>
+      <h2>
+        My Outfits
+      </h2>
+
+      <ul>
+        {outfitsList}
+      </ul>
+    </>
   )
 }
 
